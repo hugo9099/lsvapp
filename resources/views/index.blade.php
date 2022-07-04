416ba06d9b5f2c3a64062f72737f9b4ff67819cc
hugo9099 <hugo9099@yahoo.com>
2022-07-04 06:51:26 -0400

    Add content and links to Home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,15 @@
 <x-layout>
 
-    Home Page
+    <section class="jumbotron text-center">
+        <div class="container">
+            <h1>LSV App</h1>
+            <p class="lead text-muted">Small App to monitor projects and tasks assigned to users.</p>
+            <p>
+                <a href="/projects" class="btn btn-primary my-2">Projects</a>
+                <a href="/tasks" class="btn btn-primary my-2">Tasks</a>
+                <a href="/users" class="btn btn-primary my-2">Users</a>
+            </p>
+        </div>
+    </section>
 
 </x-layout>
